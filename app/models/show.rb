08b4f371edb8c27dc348f522a7c1a9a612fd95ab
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-24 18:18:03 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,11 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
   def self.most_popular_show
-    Show.highest_rating(:name)
+    Show.find_by(highest_rating)
   end
 
 end
